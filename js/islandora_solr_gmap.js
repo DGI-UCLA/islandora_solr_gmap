39acac9fcfe85476e093822699c625f055dc7291
Danny Joris <danny_joris@yahoo.co.uk>
2012-01-04 19:06:45 -0400

    preparing data to be placed on map

--- js/islandora_solr_gmap.js
+++ js/islandora_solr_gmap.js
@@ -18,6 +18,9 @@ function initialize_map() {
   // Give them something to look at while the map loads:
   init_lat_lon = "34.052234,-118.243685";
 
+  // latlong object
+  var lat_lons = Drupal.settings.islandora_solr_gmap;
+
   var latlng = new google.maps.LatLng(34.052234,-118.243685);
   var opts = {
     zoom: 14,
@@ -56,4 +59,39 @@ function initialize_map() {
   };
 
   map = new google.maps.Map(document.getElementById('islandora-solr-gmap'), opts);
+  
+//  var marker = new google.maps.Marker({
+//    position: new google.maps.LatLng(34.052234, -118.243685),
+//    map: map,
+//    title: 'Click Me'
+//  });
+  
+  
+  var total = 0;
+  var init_marker = null;
+  for (var lat_long in lat_lons) {
+    //alert(lat_lons[lat_long]);
+    
+    var ll = lat_lons[lat_long].latlong.split(" "); // set this in admin settings how this is done.
+    //var recs = lat_lons[ll];
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(parseFloat(ll[0]), parseFloat(ll[1])),
+      map: map,
+      flat: true,
+      visible: true,
+      //icon: marker_icons[recs.length > 100 ? 100 : recs.length],
+      title: lat_lons[lat_long].PID
+    });
+//    markers.push(marker);
+//    google.maps.event.addListener(marker, 'click', makeCallback(ll, marker));
+    // google.maps.event.addListener(marker, 'mouseover', makePreloadCallback(ll));
+
+//    total += recs.length;
+
+//    if (lat_lon == init_lat_lon) init_marker = marker;
+  }
+  
+  
+  
+  
 }
